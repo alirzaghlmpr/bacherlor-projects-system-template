6b25b1a295b536a4ad8bbd5eb71ce9e03d33ffa6
alirzaghlmpr <alirzaghlmpr@gmail.com>
2023-10-25 12:53:51 +0330

    dummy change to test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ProjectsTableHeaders from "./constants/ProjectsTableHeaders";
 import TableMockData from "./mocks/TableMockData";
 import LoginForm from "./components/templates/LoginForm";
 import Table from "./components/shared/Table";
+
 const App = () => {
   return <Table headers={ProjectsTableHeaders} data={TableMockData} />;
 };
