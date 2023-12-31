cd7ffbccf4af4c74adf5514df1019d0d619ca5a3
alirzaghlmpr <alirzaghlmpr@gmail.com>
2023-12-31 13:33:16 +0330

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthProvider from "./components/layouts/AuthProvider";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddProject from "./routes/AddProject";
+import UserDashboard from "./routes/UserDashboard";
+import Requests from "./routes/Requests";
+import SupervisorDashboard from "./routes/SupervisorDashboard";
+import SupervisorRequests from "./routes/SupervisorRequests";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/addproject",
+    element: <AddProject />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
 );
