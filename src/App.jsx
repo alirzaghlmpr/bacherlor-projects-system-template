c2c8e027c300f18b74517c11e1906defda636d77
alirzaghlmpr <alirzaghlmpr@gmail.com>
2024-01-01 18:00:05 +0330

    delete comments and some libs

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,7 @@
 import Root from "./routes/Root";
-import UserDashboard from "./routes/UserDashboard";
-import Requests from "./routes/Requests";
-import SupervisorDashboard from "./routes/SupervisorDashboard";
-import SupervisorRequests from "./routes/SupervisorRequests";
-import AddProject from "./routes/AddProject";
-
-import useUserStore from "./store/useUserStore";
-import shallow from "zustand/shallow";
-
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 const App = () => {
-  // const { firstName, lastName, suid, setUserInfo, token, resetUserInfo } =
-  //   useUserStore(
-  //     (state) => ({
-  //       firstName: state?.firstName,
-  //       lastName: state?.lastName,
-  //       suid: state?.suid,
-  //       setUserInfo: state?.setUserInfo,
-  //       resetUserInfo: state?.resetUserInfo,
-  //       token: state?.token,
-  //     }),
-  //     shallow
-  //   );
-  // return (
-  //   <>
-  //     <p>
-  //       {firstName}, {lastName} , {suid},{token}
-  //     </p>
-  //     <button
-  //       onClick={() =>
-  //         setUserInfo({
-  //           firstName: "ali",
-  //           lastName: "gh",
-  //           suid: "123465",
-  //           token: "token",
-  //         })
-  //       }>
-  //       change
-  //     </button>
-  //     <button onClick={resetUserInfo}>reset</button>
-  //   </>
-  // );
   return <Root />;
-
-  //   return (
-  //     <QueryClientProvider client={queryClient}>
-  //       <Example />
-  //     </QueryClientProvider>
-  //   );
-  // };
-
-  // const Example = () => {
-  //   const { data, error, isError, isPending } = useQuery({
-  //     queryKey: ["user"],
-  //     queryFn: async () => {
-  //       const response = await fetch(
-  //         "https://jsonplaceholder.typicode.com/users/1"
-  //       );
-  //       if (!response.ok) throw new Error("something goes wrong!");
-  //       return response.json();
-  //     },
-  //   });
-
-  //   if (isPending) return <p>Loading...</p>;
-  //   if (isError) return <p>Error : {error.message}</p>;
-
-  //   return <p>{JSON.stringify(data)}</p>;
 };
 
 export default App;
