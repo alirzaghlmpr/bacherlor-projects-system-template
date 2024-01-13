9670bc79164e5c3b8b263804000b06e119208ff8
Mahdi Khoramabadi <mahdi.sidsid@gmail.com>
2024-01-13 20:30:08 +0330

    Moving django codes to another repo

--- project/users/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import UserLoginView, home
-
-urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('', home, name='home'),
-]
